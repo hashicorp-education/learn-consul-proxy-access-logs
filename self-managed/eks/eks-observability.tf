09cd567d5ff2bb93119cdfa6f8d3e29db5cee84d
Eddie Rowe <74205376+eddie-rowe@users.noreply.github.com>
2023-12-05 12:17:03 -0600

    update to latest stack

--- self-managed/eks/eks-observability.tf
+++ self-managed/eks/eks-observability.tf
@@ -9,7 +9,7 @@ resource "kubernetes_namespace" "observability" {
 resource "helm_release" "loki" {
   name       = "loki"
   repository = "https://grafana.github.io/helm-charts"
-  chart      = "loki-stack"
+  chart      = "loki"
   namespace  = "observability"
 
   values = [
@@ -21,6 +21,26 @@ resource "helm_release" "loki" {
                 kubernetes_namespace.observability,
                 module.vpc,
                 helm_release.consul
+                ]
+}
+
+# Create promtail deployment
+resource "helm_release" "promtail" {
+  name       = "promtail"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "promtail"
+  namespace  = "observability"
+
+  values = [
+    templatefile("${path.module}/helm/promtail.yaml", {})
+  ]
+
+  depends_on = [module.eks.eks_managed_node_groups,
+                module.eks.aws_eks_addon,
+                kubernetes_namespace.observability,
+                module.vpc,
+                helm_release.consul,
+                helm_release.loki
                 ]
 }
 
